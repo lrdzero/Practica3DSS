7db2eccadbfca40388b06a95b9cc8539de0dd59d
lrdzero <elendil.capt.gondor@gmail.com>
2015-12-13 20:55:29 +0100

    Update de datos

--- src/com/lrdzero/jersey/todo/dao/TodoDao.java
+++ src/com/lrdzero/jersey/todo/dao/TodoDao.java
@@ -12,13 +12,13 @@ public enum TodoDao {
   
   private TodoDao() {
     
-    Todo todo = new Todo("1", "Learn REST");
-    todo.setDescription("Read http://www.vogella.com/tutorials/REST/article.html");
+    Todo todo = new Todo("Alemania", "Leopard 2");
+    todo.setDescription("http://data3.primeportal.net/tanks/peter_van_iren/leopard_2a4_nld/images/leopard_2a4_nld_02_of_15.jpg");
     contentProvider.put("1", todo);
-    todo = new Todo("2", "Do something");
-    todo.setDescription("Read complete http://www.vogella.com");
+    todo = new Todo("Rusia", "Armata");
+    todo.setDescription("https://aw.my.com/us/sites/aw.my.com/files/styles/news_body_image_1/public/u183517/8jn9gki0yse.jpg");
     contentProvider.put("2", todo);
-    todo = new Todo("3","Leopard 2");
+    todo = new Todo("Germany","Leopard 2");
     todo.setDescription("http://1.bp.blogspot.com/-cXZ57bzBeX4/TmCA8o0YMvI/AAAAAAAAG10/cbLrGPvfwmM/s1600/KMW_LEOPARD2_11.jpg");
     
   }
